6083c3127ff82e11860c05b6f6a8f721aa39e37f
Ritik Srivastava <111173076+ritiks321@users.noreply.github.com>
2026-04-18 20:06:07 +0000

    Data Types

---
+++ 01_basics/datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all Js code as newer version
+
+// alert(3+3) // We are using nodejs, not browser
+
+let name = "Ritik"
+let age = 21
+let isMarried = false
+
+// number
+// bigint
+// string => "" or ''
+// boolean => T/F
+// null => standalone value
+// undefined => value hasn't been assigned yet
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof 12)
+console.log(typeof name)
+console.log(typeof null)
+console.log(typeof undefined)
+
+
